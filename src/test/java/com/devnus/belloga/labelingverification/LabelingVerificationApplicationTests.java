545ae833946193ddffd30e45cb88d2974ab1f663
조광식 <joks7285@naver.com>
2022-07-29 02:21:43 +0900

    chore: init project

---
+++ src/test/java/com/devnus/belloga/labelingverification/LabelingVerificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devnus.belloga.labelingverification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabelingVerificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
