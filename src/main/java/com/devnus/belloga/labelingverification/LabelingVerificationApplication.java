2dc9148f8af4426bb7adf09fa5b3c32c7aeb686c
genius-jo <joks7285@daum.net>
2022-07-29 02:21:43 +0900

    chore: init project

---
+++ src/main/java/com/devnus/belloga/labelingverification/LabelingVerificationApplication.java
@@ -0,0 +1,13 @@
+package com.devnus.belloga.labelingverification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabelingVerificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabelingVerificationApplication.class, args);
+	}
+
+}
