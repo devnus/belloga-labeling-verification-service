a2cf1786546a9305a15dd9f3f9711e6d7a35045f
조광식 <joks7285@naver.com>
2022-08-12 07:13:30 +0900

    chore : Add h2, flyway, spring-batch, jpa, kafka to build.gradle

--- src/main/java/com/devnus/belloga/labelingverification/LabelingVerificationApplication.java
+++ src/main/java/com/devnus/belloga/labelingverification/LabelingVerificationApplication.java
@@ -1,9 +1,13 @@
 package com.devnus.belloga.labelingverification;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
+@EnableBatchProcessing
 public class LabelingVerificationApplication {
 
 	public static void main(String[] args) {
