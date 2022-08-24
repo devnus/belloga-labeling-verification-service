4416d73cd59b785d96e4eb9b90a24ac6af3c192c
genius-jo <joks7285@daum.net>
2022-08-24 14:43:59 +0900

    fix: Fix Kafka consumer config

--- src/main/java/com/devnus/belloga/labelingverification/common/config/KafkaConsumerConfig.java
+++ src/main/java/com/devnus/belloga/labelingverification/common/config/KafkaConsumerConfig.java
@@ -1,5 +1,6 @@
 package com.devnus.belloga.labelingverification.common.config;
 
+import com.devnus.belloga.labelingverification.verification.dto.EventLabeledData;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
@@ -22,21 +23,35 @@ public class KafkaConsumerConfig {
     @Value("${spring.kafka.consumer.enable-auto-commit}")
     private boolean AUTO_COMMIT;
 
-    @Bean
-    ConsumerFactory<String,Object> consumerFactory(){
+    private Map<String, Object> configProps() {
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class); //카프카에서 받은 키 역 직렬화
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class); //카프카에서 받은 값 역 직렬화
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, AUTO_OFFSET_RESET);
         props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, AUTO_COMMIT);
-        return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), new JsonDeserializer<>(Object.class));
+        return props;
+    }
+    @Bean
+    ConsumerFactory<String,Object> consumerFactory(){
+        return new DefaultKafkaConsumerFactory<>(configProps(), new StringDeserializer(), new JsonDeserializer<>(Object.class));
     }
 
     @Bean
     ConcurrentKafkaListenerContainerFactory<String, Object> containerFactory(){
         ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
+    @Bean
+    ConsumerFactory<String, EventLabeledData.LabelingOCRBoundingBox> eventLabeledDataLabelingOCRBoundingBoxFactory(){
+        return new DefaultKafkaConsumerFactory<>(configProps(), new StringDeserializer(), new JsonDeserializer<>(EventLabeledData.LabelingOCRBoundingBox.class));
+    }
+
+    @Bean
+    ConcurrentKafkaListenerContainerFactory<String, EventLabeledData.LabelingOCRBoundingBox> eventLabeledDataLabelingOCRBoundingBoxListener(){
+        ConcurrentKafkaListenerContainerFactory<String, EventLabeledData.LabelingOCRBoundingBox> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(eventLabeledDataLabelingOCRBoundingBoxFactory());
         return factory;
     }
 }
